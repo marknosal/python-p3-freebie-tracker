8bfa72d64a1690e2a1a10dac40d97a0351bf78ee
Mark Nosal <mark.c.nosal@gmail.com>
2023-06-26 07:27:43 -0500

    finished lab

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, create_engine
+from sqlalchemy.orm import relationship, backref, sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
+
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -9,15 +11,43 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+engine = create_engine('sqlite:///freebies.db')
+Session = sessionmaker(bind = engine)
+session = Session()
+
 class Company(Base):
     __tablename__ = 'companies'
+
+    
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship('Freebie', back_populates='company', cascade='all, delete-orphan')
+    devs = association_proxy('freebies', 'dev', creator=lambda de: Freebie(dev=de))
+
+    session = None
+
+
     def __repr__(self):
         return f'<Company {self.name}>'
+    
+    def give_freebie(self, dev, item_name, value):
+        new_freebie = Freebie(
+            item_name=item_name,
+            value=value,
+            company = self,
+            dev=dev
+        )
+        session.add(new_freebie)
+        session.commit()
+
+    @classmethod
+    def oldest_company(cls):
+        return session.query(cls).order_by(cls.founding_year).first()
+    
+
 
 class Dev(Base):
     __tablename__ = 'devs'
@@ -25,5 +55,42 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    freebies = relationship('Freebie', back_populates='dev')
+    companies = association_proxy('freebies', 'company', creator=lambda co: Freebie(company=co))
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+    def received_one(self, item_name):
+        for fb in self.freebies:
+            if fb.item_name == item_name:
+                return True
+        return False
+    
+    def give_away(self, dev, freebie):
+        if self.received_one(freebie.item_name):
+            freebie.dev = dev
+            session.add(freebie)
+            session.commit()
+            return f'{self.name} gave {freebie.id} to {dev.name}'
+        return 'I don\'t have that freebie!'
+    
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+
+    company = relationship('Company', back_populates='freebies')
+    dev = relationship('Dev', back_populates='freebies')
+
+    def __repr__(self):
+        return f'<Freebie {self.id}>'
+
+    def print_details(self):
+        return f'{self.dev.name} owns a {self.item_name} from {self.company.name}'
+    
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,53 @@
 #!/usr/bin/env python3
 
-# Script goes here!
+from faker import Faker
+import random
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Dev, Company, Freebie
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///freebies.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(Company).delete()
+    session.query(Dev).delete()
+    session.query(Freebie).delete()
+
+    fake = Faker()
+
+    devs = []
+    for i in range(25):
+        dev = Dev(
+            name = fake.unique.name()
+        )
+        session.add(dev)
+        session.commit()
+        devs.append(dev)
+
+    companies = []
+    for i in range(10):
+        company = Company(
+            name = fake.unique.name(),
+            founding_year = fake.date_between('-30y', 'today')
+        )
+        session.add(company)
+        session.commit()
+        companies.append(company)
+
+    freebies = []
+    for i in range(10):
+        random_company = random.choice(companies)
+        random_dev = random.choice(devs)
+        freebie = Freebie(
+            item_name = fake.unique.name(),
+            value = random.randint(0, 100),
+            company = random_company,
+            dev = random_dev
+        )
+        session.add(freebie)
+        session.commit()
+    session.close()
